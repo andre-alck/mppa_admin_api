fbaf1f325add036ec0c7d0e7c99bea541d794774
andre-alck <andrealck1@gmail.com>
2022-10-31 14:56:23 -0300

    feat: init commit

---
+++ src/main/java/com/api/mppaadmin/MppaAdminApplication.java
@@ -0,0 +1,13 @@
+package com.api.mppaadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MppaAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MppaAdminApplication.class, args);
+	}
+
+}
